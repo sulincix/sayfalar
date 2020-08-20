9c4fab70f27f9fd2be1d60ae0ab5d27323daad47
sulincix <parduscix@yandex.ru>
2020-08-20 07:48:24 +0000

    index

--- update.sh
+++ update.sh
@@ -3,8 +3,14 @@ for j in man odt html
 do
 	rm -rf $j
 	mkdir -p $j
-	for i in $(ls rst/*.rst | sed "s/.rst$//g" | sed "s/rst\///g")
+	for i in $(ls rst/*.rst | sed "s/.rst$//g" | sed "s/rst\///g" | sort)
 	do
 		rst2$j rst/$i.rst > $j/$i.$j
 	done
 done
+echo "<head><title>Sayfalar</title></head><body>" > index.html
+for i in $(ls html | sed "s/.html//g" | sort)
+do
+	echo -e "=> <a href=\"html/$i.html\">$i</a><br>" >> index.html
+done
+echo "</body>" >> index.html
